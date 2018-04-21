e4b689e10d4445367f2d0b5425daaaa1bd2cbac0
David Cruz <dcruz24@illinois.edu>
2018-04-21 18:51:25 -0500

    MP7 Music App- Testing Commitment

--- app/src/main/java/edu/illinois/cs/cs125/musiclyricsapp/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/musiclyricsapp/MainActivity.java
@@ -1,13 +1,16 @@
 package edu.illinois.cs.cs125.musiclyricsapp;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(Tag, "App Has Started");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
